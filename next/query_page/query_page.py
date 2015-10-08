bb9415312e86fcf140a5784788a16adcc392a8ae
scottsievert <sieve121@umn.edu>
2015-10-08 15:28:40 -0500

    implements query page as popup

--- next/query_page/query_page.py
+++ next/query_page/query_page.py
@@ -10,13 +10,18 @@ import os
 keychain = KeyChain()
 query_page = Blueprint('query_page', __name__, template_folder="templates", static_folder="static")
 
-@query_page.route('/query_page/<page>') 
-@query_page.route('/query_page/<page>/<exp_uid>/<widget_key>') 
+@query_page.route('/query_page/<page>')
+@query_page.route('/query_page/<page>/<exp_uid>/<widget_key>')
 def load_page(page, exp_uid=None, widget_key=None):
     experiment = resource_manager.get_experiment(exp_uid)
+
     num_tries = experiment['num_tries']
+
     app_template = page+'.html'
     print "app template is %s"%(app_template)
     host_url = "http://"+constants.NEXT_BACKEND_GLOBAL_HOST+":"+constants.NEXT_BACKEND_GLOBAL_PORT
-    return render_template(app_template, host_url=host_url, exp_uid=exp_uid, widget_key=widget_key, num_tries=num_tries), 200, {'Cache-Control':'private, max-age=0, no-cache, no-store'}
+
+    return render_template(app_template, host_url=host_url, exp_uid=exp_uid,
+                          widget_key=widget_key, num_tries=num_tries), \
+           200, {'Cache-Control':'private, max-age=0, no-cache, no-store'}
 
