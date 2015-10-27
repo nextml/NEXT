1253102c86ef7de0b5410ff616013bfc23c84072
jain <lalitkumarj@gmail.com>
2015-10-27 16:41:39 -0500

    Having relative path issues.

--- next/database/database_lib.py
+++ next/database/database_lib.py
@@ -8,11 +8,11 @@ import next.constants as constants
 
 def make_mongodump(name):
     print "in make_mongodump"
-    subprocess.call(('/usr/bin/mongodump -vvvvv --host {hostname}:{port}'
+    subprocess.call(('/usr/bin/mongodump -vvvvv --host {hostname}:{port} '
                      '--out dump/mongo_dump').format(hostname=constants.MONGODB_HOST,
                                                       port=constants.MONGODB_PORT),
                     shell=True)
-    subprocess.call(('tar czf {path}/{name}'
+    subprocess.call(('tar czf {path}/{name} '
                      'dump/mongo_dump').format(path='/dump',
                                                 name=name),
                     shell=True)
