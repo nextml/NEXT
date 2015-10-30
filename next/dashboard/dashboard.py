2f573007ce56df677a7495c98bb91732f1d3c272
Kevin Jamieson <kevin.g.jamieson@gmail.com>
2015-10-30 12:40:06 -0700

    Added basic statistics to dashboard header

--- next/dashboard/dashboard.py
+++ next/dashboard/dashboard.py
@@ -9,6 +9,7 @@ from flask import Blueprint, render_template, url_for
 from jinja2 import Environment, PackageLoader, ChoiceLoader
 
 import next.constants as constants
+import next.utils as utils
 import next.database_client.PermStore as PermStore
 from next.api.resource_manager import ResourceManager
 
@@ -74,6 +75,20 @@ def experiment_dashboard(exp_uid, app_id, exp_key):
     # Not a particularly good way to do this. 
     alg_label_list = rm.get_algs_for_exp_uid(exp_uid)
 
+    exp_start_data = rm.get_app_exp_uid_start_date(exp_uid)+' UTC'
+    participant_uids = rm.get_participant_uids(exp_uid)
+    num_participants = len(participant_uids)
+    last_datetime = None
+    num_queries = 0
+    for participant_uid in participant_uids:
+      queries = rm.get_participant_data(participant_uid, exp_uid)
+      num_queries += len(queries)
+
+      this_datetime = utils.str2datetime(queries[-1]['timestamp_query_generated'])
+      if last_datetime==None or this_datetime>last_datetime:
+        last_datetime = this_datetime
+    last_activity = utils.datetime2str(last_datetime)+' UTC'
+
     # Migrate this code to use keychain
     docs,didSucceed,message = db.getDocsByPattern('next_frontend_base',
                                                   'keys',
@@ -102,7 +117,12 @@ def experiment_dashboard(exp_uid, app_id, exp_key):
                            alg_list=alg_list,
                            host_url=host_url,
                            perm_key=perm_key,
-                           url_for=url_for)
+                           url_for=url_for,
+                           exp_start_data=exp_start_data,
+                           num_participants=num_participants,
+                           num_queries=num_queries,
+                           last_activity=last_activity
+                           )
 
 
 
