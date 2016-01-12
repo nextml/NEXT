e805b32f3ca078e709cf7360504c40e55a902342
scottsievert <sieve121@umn.edu>
2016-01-12 15:25:49 -0600

    moves over more of App.py (and cleans Verifier.py)

--- next/apps/App.py
+++ next/apps/App.py
@@ -7,75 +7,113 @@ import traceback
 from next.resource_client.ResourceClient import ResourceClient
 import next.utils as utils
 from next.apps.AppPrototype import AppPrototype
+import next.apps.Verifier as Verifier
 from next.apps.PoolBasedTripletMDS.dashboard.Dashboard import PoolBasedTripletMDSDashboard
+import next.constants
+git_hash = next.constants.GIT_HASH
 
 class PoolBasedTripletMDS(AppPrototype):
-    def __init__(self): 
+    def __init__(self):
         self.app_id = 'PoolBasedTripletMDS'
 
     def daemonProcess(self,exp_uid,args_json,db,ell):
         pass
 
     def initExp(self,exp_uid,args_json,db,ell):
-        success, args, message = verifier(args_json):
-        if not success:
-            return False, "Failed to verify: {}".format(message)
+        try:
+            app_id = self.app_id
 
-        # remove any reminants of an experiment if it exists
-        didSucceed,message = db.delete_docs_with_filter('experiments_admin',{'exp_uid':exp_uid})
-        didSucceed,message = db.delete_docs_with_filter(app_id+':experiments',{'exp_uid':exp_uid})
-        didSucceed,message = db.delete_docs_with_filter(app_id+':queries',{'exp_uid':exp_uid})
-        didSucceed,message = db.delete_docs_with_filter(app_id+':participants',{'exp_uid':exp_uid})
-        didSucceed,message = db.delete_docs_with_filter(app_id+':algorithms',{'exp_uid':exp_uid})
-        
-        didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-CALL',{'exp_uid':exp_uid})
-        didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-RESPONSE',{'exp_uid':exp_uid})
-        didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-EXCEPTION',{'exp_uid':exp_uid})
-        didSucceed,message = ell.delete_logs_with_filter(app_id+':ALG-DURATION',{'exp_uid':exp_uid})
-        didSucceed,message = ell.delete_logs_with_filter(app_id+':ALG-EVALUATION',{'exp_uid':exp_uid})
-        
-        # add indexes (only adds them if they do not already exist)
-        didSucceed,message = db.ensure_index('experiments_admin',{'exp_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':experiments',{'exp_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':queries',{'query_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':queries',{'exp_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':queries',{'alg_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':queries',{'participant_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':participants',{'participant_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':participants',{'exp_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':algorithms',{'alg_uid':1})
-        didSucceed,message = db.ensure_index(app_id+':algorithms',{'exp_uid':1})
+            args_json, success, messages = Verifier.verify(args_json):
+            if not success:
+                # TODO: Turn this into an exception
+                return False, "Failed to verify: {}".format(" \n".join(messages))
 
-        didSucceed,message = ell.ensure_index(app_id+':APP-CALL',{'exp_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-CALL',{'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-CALL',{'exp_uid':1,'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-CALL',{'exp_uid':1,'task':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-RESPONSE',{'exp_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-RESPONSE',{'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-RESPONSE',{'exp_uid':1,'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-RESPONSE',{'exp_uid':1,'task':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-EXCEPTION',{'exp_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-EXCEPTION',{'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-EXCEPTION',{'exp_uid':1,'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':APP-EXCEPTION',{'exp_uid':1,'task':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-DURATION',{'exp_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-DURATION',{'alg_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-DURATION',{'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-DURATION',{'exp_uid':1,'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-DURATION',{'alg_uid':1,'task':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-EVALUATION',{'exp_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-EVALUATION',{'alg_uid':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-EVALUATION',{'timestamp':1})
-        didSucceed,message = ell.ensure_index(app_id+':ALG-EVALUATION',{'exp_uid':1,'timestamp':1})
-        
-        db.set('experiments_admin',exp_uid,'exp_uid',exp_uid)
-        db.set('experiments_admin',exp_uid,'app_id',app_id)
-        db.set('experiments_admin',exp_uid,'start_date',utils.datetime2str(utils.datetimeNow()))
+            # Convert the args JSON to an args dict
+            try:
+                args_dict = json.loads(args_json)
+            except:
+                error = "%s.initExp input args_json is in improper format" % self.app_id
+                return '{}', False, error
 
-        log_entry = { 'exp_uid':exp_uid,'task':'initExp','json':args_json,'timestamp':utils.datetimeNow() } 
-        ell.log( app_id+':APP-CALL', log_entry  )
+            # remove any reminants of an experiment if it exists
+            didSucceed,message = db.delete_docs_with_filter('experiments_admin',{'exp_uid':exp_uid})
+            didSucceed,message = db.delete_docs_with_filter(app_id+':experiments',{'exp_uid':exp_uid})
+            didSucceed,message = db.delete_docs_with_filter(app_id+':queries',{'exp_uid':exp_uid})
+            didSucceed,message = db.delete_docs_with_filter(app_id+':participants',{'exp_uid':exp_uid})
+            didSucceed,message = db.delete_docs_with_filter(app_id+':algorithms',{'exp_uid':exp_uid})
 
-        
+            didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-CALL',{'exp_uid':exp_uid})
+            didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-RESPONSE',{'exp_uid':exp_uid})
+            didSucceed,message = ell.delete_logs_with_filter(app_id+':APP-EXCEPTION',{'exp_uid':exp_uid})
+            didSucceed,message = ell.delete_logs_with_filter(app_id+':ALG-DURATION',{'exp_uid':exp_uid})
+            didSucceed,message = ell.delete_logs_with_filter(app_id+':ALG-EVALUATION',{'exp_uid':exp_uid})
+
+            # Database call that creates the experiment
+            db.set('experiments_admin',exp_uid,'exp_uid',exp_uid)
+            db.set('experiments_admin',exp_uid,'app_id',app_id)
+            db.set('experiments_admin',exp_uid,'start_date',utils.datetime2str(utils.datetimeNow()))
+
+            log_entry = { 'exp_uid':exp_uid,'task':'initExp','json':args_json,'timestamp':utils.datetimeNow() }
+            ell.log(app_id+':APP-CALL', log_entry  )
+
+
+            # assign uid to each algorithm and save it
+            # scott: alg_list copied from line 201 in PoolBasedTripletMDS.py
+            alg_list = args_dict['alg_list']
+            for algorithm in alg_list:
+                alg_id = algorithm['alg_id']
+                alg_uid = utils.getNewUID()
+                algorithm['alg_uid'] = alg_uid
+
+                db.set(app_id+':algorithms',alg_uid,'alg_id',alg_id)
+                db.set(app_id+':algorithms',alg_uid,'alg_uid',alg_uid)
+                db.set(app_id+':algorithms',alg_uid,'exp_uid',exp_uid)
+
+            # Setting experiment parameters in the database
+            db.set(app_id+':experiments', exp_uid, 'exp_uid', exp_uid)
+            db.set(app_id+':experiments', exp_uid, 'app_id', app_id)
+            db.set(app_id+':experiments', exp_uid, 'n', args_dict['n'])
+            db.set(app_id+':experiments', exp_uid, 'd', args_dict['d'])
+            db.set(app_id+':experiments', exp_uid, 'failure_probability', args_dict['failure_probability'])
+            db.set(app_id+':experiments', exp_uid, 'alg_list', alg_list)
+            db.set(app_id+':experiments', exp_uid, 'algorithm_management_settings', args_dict['algorithm_management_settings'])
+            db.set(app_id+':experiments', exp_uid, 'participant_to_algorithm_management', participant_to_algorithm_management)
+            db.set(app_id+':experiments', exp_uid, 'instructions', args_json['instructions'])
+            db.set(app_id+':experiments', exp_uid, 'debrief', args_json['debrief'])
+            db.set(app_id+':experiments', exp_uid, 'num_tries', args_json['num_tries'])
+            db.set(app_id+':experiments', exp_uid, 'git_hash', git_hash)
+
+            # now create intitialize each algorithm
+            for algorithm in alg_list:
+                alg_id = algorithm['alg_id']
+                alg_uid = algorithm['alg_uid']
+                params = algorithm.get('params',None)
+
+                # get sandboxed database for the specific app_id,alg_uid,exp_uid - closing off the rest of the database to the algorithm
+                rc = ResourceClient(app_id,exp_uid,alg_uid,db)
+
+                # get specific algorithm to make calls to
+                alg = utils.get_app_alg(self.app_id,alg_id)
+
+                # call initExp
+                didSucceed,dt = utils.timeit(alg.initExp)(resource=rc,n=n,d=d,failure_probability=delta,params=params)
+
+                log_entry = { 'exp_uid':exp_uid,'alg_uid':alg_uid,'task':'initExp','duration':dt,'timestamp':utils.datetimeNow() }
+                ell.log( app_id+':ALG-DURATION', log_entry  )
+
+            response_json = '{}'
+
+            log_entry = { 'exp_uid':exp_uid,'task':'initExp','json':response_json,'timestamp':utils.datetimeNow() }
+            ell.log( app_id+':APP-RESPONSE', log_entry  )
+
+
+
+            return response_json,True,''
+        except Exception, err:
+            return '{}', False, error
+
+
+
   def getQuery(self,exp_uid,args_json,db,ell):
       pass
 
@@ -88,4 +126,4 @@ class PoolBasedTripletMDS(AppPrototype):
   def getStats(self,exp_uid,args_json,db,ell):
       pass
 
-    
+
--- next/apps/Verifier.py
+++ next/apps/Verifier.py
@@ -1,10 +1,12 @@
 import yaml, json
+import random
+import numpy
 
 def verify(input_dict, reference_dict):
     """
     Returns: modified_input, success, list_of_errors
 
-    where: 
+    where:
     - modified_input is the input populated with default values where applicable
     - success is a boolean true if there were no problems and false otherwise
     - list_of_errors is as in verify_helper
@@ -18,10 +20,10 @@ def verify(input_dict, reference_dict):
     # modify input_dict as needed and add any errors to ans as dicts
     # with the format: {"name":problem_key, "message":"what is wrong"}
 
-    
-    
+
+
     return input_dict, len(ans) == 0, ans
-    
+
 def verify_helper(name, input_element, reference_dict):
     """
     Returns: modified_input,list_of_errors
@@ -48,12 +50,12 @@ def verify_helper(name, input_element, reference_dict):
                 for k in input_element:
                     input_element[k],temp_ans = verify_helper(name + '/' + k, input_element[k],reference_dict['values'][str(k)])
                     ans += temp_ans
-                
+
     elif reference_dict['type'] == 'list':
         for i in range(len(input_element)):
             input_element[i],temp_ans = verify_helper(name+'/'+str(i), input_element[i], reference_dict['values'])
             ans += temp_ans
-                
+
     elif reference_dict['type'] == 'num':
         if not isinstance(input_element, (int, long, float)):
             ans += [{"name":name, "message":"invalid number"}]
@@ -76,7 +78,7 @@ def verify_helper(name, input_element, reference_dict):
                 input_element = reference_dict['set']
             else:
                 ans += [{"name":name, "message":"no argument provided for 'oneof' arg"}]
-                
+
     elif reference_dict['type'] == 'target':
         pass
     elif reference_dict['type'] == 'targetset':
@@ -85,22 +87,10 @@ def verify_helper(name, input_element, reference_dict):
         pass
 
     return input_element,ans
-    
+
 def compare_dict_keys(d1, d2):
     """
     Returns [things in d1 not in d2, things in d2 not in d1]
     """
     return [k for k in d1 if not k in d2],[k for k in d2 if not k in d1]
 
-
-# with open("old_dictionary.py") as f:
-#     d = json.loads(f.read())
-    
-# with open("PoolBasedTripletMDS.yaml") as f:
-#     ref = yaml.load(f.read())
-
-# d= {'initExp':d}
-    
-# filled, success, message = verify(d, ref)
-# if(not success):
-#     print("\n".join([m['name'] + ": "+m['message'] for m in message]))
