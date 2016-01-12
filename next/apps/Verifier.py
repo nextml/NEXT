f46fc8acae2ad27d3f7999d7b062096336d1b4f5
DR <daniel3735928559@gmail.com>
2016-01-11 21:06:07 -0600

    verifier

---
+++ next/apps/Verifier.py
@@ -0,0 +1,73 @@
+import yaml, json
+
+def verify(input_dict, reference_dict):
+    ans = []
+    for k in reference_dict:
+        input_dict, temp_ans = verify_helper(k, input_dict[k], reference_dict[k])
+        ans += temp_ans
+    return input_dict, len(ans) == 0, ans
+    
+def verify_helper(name, input_element, reference_dict):
+    """
+    Returns: modified_input,list_of_errors
+
+    where list of errors is: [{name: name, message: ..., 
+    """
+    ans = []
+    if reference_dict['type'] == 'dict':
+        l1,l2 = compare_dict_keys(input_element, reference_dict['values'])
+        if len(l1) > 0:
+            ans += [{"name":name, "message":"extra keys in input: " + ",".join(l1)}]
+        else:
+            ok = True
+            for k in l2:
+                if 'default' in reference_dict['values'][k]:
+                    input_element[k] = reference_dict['values'][k]['default']
+                elif not 'required' in reference_dict['values'][k] or reference_dict['values'][k]['required'] == False:
+                    ans += [{"name":name+'/'+k, "message":"required key is absent"}]
+                    ok = False
+            if(ok):
+                for k in input_element:
+                    input_element[k],temp_ans = verify_helper(name + '/' + k, input_element[k],reference_dict['values'][str(k)])
+                    ans += temp_ans
+                
+    elif reference_dict['type'] == 'list':
+        for i in range(len(input_element)):
+            input_element[i],temp_ans = verify_helper(name+'/'+str(i), input_element[i], reference_dict['values'])
+            ans += temp_ans
+                
+    elif reference_dict['type'] == 'num':
+        if isinstance(input_element, (int, long, float)):
+            ans += [{"name":name, "message":"invalid number"}]
+
+    elif reference_dict['type'] == 'str':
+        if isinstance(input_element, str):
+            ans += [{"name":name, "message":"invalid string"}]
+
+    elif reference_dict['type'] == 'target':
+        pass
+    elif reference_dict['type'] == 'targetset':
+        pass
+    elif reference_dict['type'] == 'targetmeta':
+        pass
+
+    return input_element,ans
+    
+def compare_dict_keys(d1, d2):
+    """
+    Returns [things in d1 not in d2, things in d2 not in d1]
+    """
+    return [k for k in d1 if not k in d2],[k for k in d2 if not k in d1]
+
+
+# with open("old_dictionary.py") as f:
+#     d = json.loads(f.read())
+    
+# with open("PoolBasedTripletMDS.yaml") as f:
+#     ref = yaml.load(f.read())
+
+# d= {'initExp':d}
+    
+# filled, success, message = verify(d, ref)
+# if(not success):
+#     print("\n".join([m['name'] + ": "+m['message'] for m in message]))
