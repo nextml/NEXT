d15322a3e38271d60982415bc198ed66b24fdf25
DR <daniel3735928559@gmail.com>
2016-01-26 13:21:43 -0600

    Verifier made more robust

--- next/apps/Verifier.py
+++ next/apps/Verifier.py
@@ -12,11 +12,9 @@ def verify(input_dict, reference_dict):
     - success is a boolean true if there were no problems and false otherwise
     - list_of_errors is as in verify_helper
     """
-    ans = []
-    for k in input_dict:
-        _, temp_ans = verify_helper(k, input_dict[k], reference_dict[k])
-        ans += temp_ans
-
+    
+    input_dict, ans = verify_helper("", input_dict, {'type':'dict','values':reference_dict})
+    
     # Any further custom verification goes below this line, which may
     # modify input_dict as needed and add any errors to ans as dicts
     # with the format: {"name":problem_key, "message":"what is wrong"}
@@ -65,26 +63,32 @@ def verify_helper(name, input_element, reference_dict):
     """
     ans = []
     if reference_dict['type'] == 'dict':
-        l1,l2 = compare_dict_keys(input_element, reference_dict['values'])
-        if len(l1) > 0:
-            ans += [{"name":name, "message":"extra keys in input: " + ",".join(l1)}]
+        if not isinstance(input_element, (dict)):
+            ans += [{"name":name, "message":"invalid dict"}]
         else:
-            ok = True
-            for k in l2:
-                if 'set' in reference_dict['values'][k]:
-                    input_element[k] = reference_dict['values'][k]['set']
-                elif (not 'optional' in reference_dict['values'][k]) or reference_dict['values'][k]['optional'] == False:
-                    ans += [{"name":name+'/'+k, "message":"required key is absent"}]
-                    ok = False
-            if(ok):
-                for k in input_element:
-                    input_element[k], temp_ans = verify_helper(name + '/' + k, input_element[k], reference_dict['values'][str(k)])
-                    ans += temp_ans
+            l1,l2 = compare_dict_keys(input_element, reference_dict['values'])
+            if len(l1) > 0:
+                ans += [{"name":name, "message":"extra keys in input: " + ",".join(l1)}]
+            else:
+                ok = True
+                for k in l2:
+                    if 'set' in reference_dict['values'][k]:
+                        input_element[k] = reference_dict['values'][k]['set']
+                    elif (not 'optional' in reference_dict['values'][k]) or reference_dict['values'][k]['optional'] == False:
+                        ans += [{"name":name+'/'+k, "message":"required key is absent"}]
+                        ok = False
+                if(ok):
+                    for k in input_element:
+                        input_element[k], temp_ans = verify_helper(name + '/' + k, input_element[k], reference_dict['values'][str(k)])
+                        ans += temp_ans
 
     elif reference_dict['type'] == 'list':
-        for i in range(len(input_element)):
-            input_element[i],temp_ans = verify_helper(name+'/'+str(i), input_element[i], reference_dict['values'])
-            ans += temp_ans
+        if not isinstance(input_element, (list)):
+            ans += [{"name":name, "message":"invalid list"}]
+        else:
+            for i in range(len(input_element)):
+                input_element[i],temp_ans = verify_helper(name+'/'+str(i), input_element[i], reference_dict['values'])
+                ans += temp_ans
 
     elif reference_dict['type'] == 'num':
         if not isinstance(input_element, (int, long, float)):
@@ -143,16 +147,16 @@ if __name__ == "__main__":
                         'test_alg_label': 'Test'}],
           'algorithm_management_settings': {'mode': 'fixed_proportions',
                                             'params': [{'alg_label': 'Test',
-                                                                        'proportion': 0.2},
-                                                                       {'alg_label': 'Random',
-                                                                        'proportion': 0.26666666666666666},
-                                                                       {'alg_label': 'Uncertainty Sampling',
-                                                                        'proportion': 0.26666666666666666},
-                                                                       {'alg_label': 'Crowd Kernel',
-                                                                        'proportion': 0.26666666666666666}]},
-          'd': 2,
-          'failure_probability': 0.01,
-          'participant_to_algorithm_management': 'one_to_many'},
+                                                        'proportion': 0.2},
+                                                       {'alg_label': 'Random',
+                                                        'proportion': 0.26666666666666666},
+                                                       {'alg_label': 'Uncertainty Sampling',
+                                                        'proportion': 0.26666666666666666},
+                                                       {'alg_label': 'Crowd Kernel',
+                                                        'proportion': 0.26666666666666666}]},
+            'd': 2,
+            'failure_probability': 0.01,
+            'participant_to_algorithm_management': 'one_to_many'},
           }
 
     # ground truth; this dictionary is assumed to be right
