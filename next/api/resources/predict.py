473fa3008a7f3ab8879254f1670915c5e641e671
Scott Sievert <me@scottsievert.com>
2016-01-26 15:18:08 -0600

    fixes typo in /experiment/predict

--- next/api/resources/predict.py
+++ next/api/resources/predict.py
@@ -1,7 +1,7 @@
 """
-next_backend Prediction Resource 
+next_backend Prediction Resource
 author: Christopher Fernandez, Lalit Jain
-Predict resource for handling restful predictions with next_backend. 
+Predict resource for handling restful predictions with next_backend.
 """
 from flask import Flask
 from flask.ext import restful
@@ -21,7 +21,7 @@ broker = next.broker.broker.JobBroker()
 keychain = KeyChain()
 
 # Request parser. Checks that necessary dictionary keys are available in a given resource.
-# We rely on learningLib functions to ensure that all necessary arguments are available and parsed. 
+# We rely on learningLib functions to ensure that all necessary arguments are available and parsed.
 post_parser = reqparse.RequestParser(argument_class=APIArgument)
 
 meta_error = {
@@ -35,7 +35,7 @@ meta_success = {
     'status':'OK',
     'code':200
 }
-    
+
 class Predict(Resource):
     def post(self):
         """
@@ -52,29 +52,29 @@ class Predict(Resource):
         {
 	        exp_uid: exp_uid,
         	app_id: app_id,
-        	args: { }        
+        	args: { }
             }
         }
 
         **Example response**:
 
         .. sourcecode:: http
-        
+
         HTTP/1.1 200 OK
         Vary: Accept
         Content-Type: application/json
 
-        {   
+        {
         }
 
-        
+
         :statuscode 200: prediction successfully accessed
         :statuscode 400: prediction retrieval error
-        """        
+        """
         post_parser.add_argument('exp_uid', type=str, required=True,help="Experiment ID Required.")
         post_parser.add_argument('exp_key', type=str, required=True,help="Experiment ID Required.")
         post_parser.add_argument('args', type=dict, required=True,help="Experiment Args Required.")
-        
+
 
         # Validate args with post_parser
         args_data = post_parser.parse_args()
@@ -83,15 +83,15 @@ class Predict(Resource):
         exp_key = args_data["exp_key"]
         if not keychain.verify_exp_key(exp_uid, exp_key):
             return api_util.attach_meta({}, api_util.verification_dictionary), 401
-                    
+
         # Fetch app_id data from resource manager
-        app_id = resource_manager.get_app_id(exp_uid) 
+        app_id = resource_manager.get_app_id(exp_uid)
         # Args from dict to json type
         args_json = json.dumps(args_data["args"])
-        # Execute getQuery 
+        # Execute getQuery
         response_json,didSucceed,message = broker.applyAsync(app_id,exp_uid,"predict",args_json)
-        
+
         if didSucceed:
-          return meta_attach(response_json,meta_success), 200
+          return attach_meta(response_json,meta_success), 200
         else:
-          return meta_attach({},custom_errors['PredictRetrievalError'], backend_error=message),400
+          return attach_meta({},custom_errors['PredictRetrievalError'], backend_error=message),400
