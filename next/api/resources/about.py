3373449f3d5ba0d68953387ee590edd2234a79fd
Scott Sievert <me@scottsievert.com>
2016-06-09 14:34:56 -0500

    cleans up utils and closes #116
    I’ve tested this all changes and have verified that experiment launch
    still works. All deletions were deleted after grep returned only
    defined in one place (or commented out in tests)

--- next/api/resources/about.py
+++ next/api/resources/about.py
@@ -10,17 +10,25 @@ from flask.ext.restful import Resource, reqparse
 
 
 from next.utils import utils
-import next.api.api_util as api_util 
+import next.api.api_util as api_util
+import yaml
+
+
 
 meta_success = {'status':'OK', 'code':200}
 
 class About(Resource):
     def get(self, app_id=None):
         if app_id:
-            return api_util.attach_meta(utils.get_app_info_object(app_id),
-                                        meta_success), 200
+            apps_path = 'next/apps/Apps/'
+            filename = apps_path + '{0}/{0}.yaml'.format(app_id)
+
+            info = yaml.load(open(filename, 'rb'))
+            args = info['initExp']['values']['args']['values']
+
+            return api_util.attach_meta(args, meta_success), 200
+
         message = ('Welcome to the next.discovery system.\n '
                    'Available apps {}'.format(', '.join(utils.get_supported_apps())))
-        return api_util.attach_meta({},
-                                    meta_success,
-                                    message=message)
+
+        return api_util.attach_meta({}, meta_success, message=message)
