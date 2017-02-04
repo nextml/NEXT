b51d7075a6915388382f5d41d45eabd21df2bc80
Scott Sievert <me@scottsievert.com>
2017-02-04 15:26:42 -0600

    BUG: freeze docker versions
    Used https://github.com/docker/compose/issues/4425 and
    https://docs.docker.com/engine/installation/linux/ubuntu/

--- ec2/templates/setup.sh
+++ ec2/templates/setup.sh
@@ -14,15 +14,26 @@ apt-get install -y \
 
 # python libraries (for convenience and test scripts)
 pip install \
-	requests==2.5.2 \
-	boto \
-	yaml
+    requests==2.5.2 \
+    boto \
+    yaml
 
 # tools for increased stability of docker volumes
 apt-get install -y linux-image-extra-$(uname -r) aufs-tools  
 
 # downloads and installs docker
-curl -sSL https://get.docker.com/ | sh
+# curl -sSL https://get.docker.com/ | sh  # only meant for testing >:[
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+sudo apt-get update
+sudo apt-get install -y curl linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y apt-transport-https ca-certificates
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get autoremove libltdl7
+sudo sudo apt-get install -y docker-engine=1.10.3-0~trusty
 
 # orchestrates docker containers
-pip install docker-compose
+pip install docker-compose==1.10.0
