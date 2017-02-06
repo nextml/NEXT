efb4b51c935b987c4fc1086267051431adbbf831
Scott Sievert <me@scottsievert.com>
2017-02-06 17:26:01 -0600

    ENH: adds CardinalBandits alg, KLUCB (joint work with Ervin)
    * This is a one line change from LilUCB.
    * technical paper forthcoming but authors are listed.
    * tested KLUCB in comparison with LilUCB. 5k arms, 100 simultaneous
    “users” with response time mean of 0.1 seconds, 585k responses.
    * Network delay nearly identical with LilUCB.
    * the async background jobs to update UCBs took 5x longer than
    LilUCB
    * Better performance in terms of sample complexity, both theory and
    empirical

--- apps/CardinalBanditsPureExploration/tests/test_api.py
+++ apps/CardinalBanditsPureExploration/tests/test_api.py
@@ -18,17 +18,17 @@ except:
         import test_utils
 
 
-def test_api(assert_200=True, num_arms=5,
-             num_experiments=1, num_clients=10, total_pulls=5):
+def test_api(assert_200=True, num_arms=50,
+             num_experiments=1, num_clients=100, total_pulls=25):
     app_id = 'CardinalBanditsPureExploration'
     true_means = numpy.array(range(num_arms)[::-1])/float(num_arms)
 
-    pool = Pool(processes=num_clients)
+    pool = Pool(processes=min(100, num_clients))
 
     # input test parameters
     n = num_arms
     delta = 0.05
-    supported_alg_ids = ['LilUCB', 'RoundRobin']
+    supported_alg_ids = ['LilUCB', 'KLUCB', 'RoundRobin']
 
     labels = [{'label':'bad','reward':1.},{'label':'neutral','reward':2.},{'label':'good','reward':3.}]
 
