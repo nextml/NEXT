3ad7c18ab1fcc7456b64167d8fb46c6185954e32
Liam Marshall <limarshall@wisc.edu>
2018-02-19 13:28:26 -0600

    docker_up.sh: do not use absolute paths

--- local/docker_up.sh
+++ local/docker_up.sh
@@ -31,26 +31,13 @@ fi
 	# from https://code.google.com/p/lusca-cache/issues/detail?id=89#c4
 	# http://stackoverflow.com/questions/11190595/repeated-post-request-is-causing-error-socket-error-99-cannot-assign-reques
 
-function abspath() {
-    if [ -d "$1" ]; then
-        echo "$(cd "$1"; pwd)"
-    elif [ -f "$1" ]; then
-        # file
-        if [[ $1 == */* ]]; then
-            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
-        else
-            echo "$(pwd)/$1"
-        fi
-    fi
-}
-
 echo "Changing next_worker_startup.sh permissions..."
 if [ "$#" -lt 2 ]; then
-    dir=$(abspath ..);
+    dir="..";
     chmod +x ../next/broker/next_worker_startup.sh
     echo "No path to NEXT provided.  Assuming ../";
 else
-    dir=$(abspath $2);
+    dir="$2";
     echo "ERROR: could not change permissions! Run 'chmod +x /path/to/.../next/broker/next_worker_startup.sh' yourself"
     echo "Using $1 as path to NEXT.";
 fi
