d58cb6bc566d7357bc0015cf7a882daabd1504e7
dconathan <dconathan@gmail.com>
2018-09-06 16:09:17 -0500

    Give Butler.Collections and Butler.Memory the python dict-api (#208)
    Allows you to use dict-like api for Collections.  e.g. `butler.experiment["foo"] = "bar"` instead of `butler.experiment.set(key="foo", value="bar")`.
    
    New tests in Tests app to test this.
    
    I also refactored the Tests app a little more.  It probably could be cleaned up a bunch more to do more unit test-y things but it's good for now.

--- apps/Tests/algs/TestAlg.py
+++ apps/Tests/algs/TestAlg.py
@@ -1,24 +1,22 @@
+from apps.Tests.tests.test_api import set_and_get_alg, get_alg, get_exp
+
 
 class MyAlg:
-    def initExp(self, butler):
-        butler.algorithms.set(key='algorithms_foo', value='algorithms_bar')
-
-        return True
+    def initExp(self, butler, dummy):
+        get_exp(butler)
+        set_and_get_alg(butler)
+        return "return_init_exp"
 
     def getQuery(self, butler):
-
-        assert butler.experiment.get(key='experiment_foo') == 'experiment_bar'
-        assert butler.algorithms.get(key='algorithms_foo') == 'algorithms_bar'
-
-        return True
+        get_alg(butler)
+        return "return_get_query"
 
     def processAnswer(self, butler):
-
-        assert butler.experiment.get(key='experiment_foo') == 'experiment_bar'
-        assert butler.algorithms.get(key='algorithms_foo') == 'algorithms_bar'
-
-        return True
+        get_alg(butler)
+        return "return_process_answer"
 
     def getModel(self, butler):
+        get_alg(butler)
+        return "return_process_answer"
 
-        return True
+
